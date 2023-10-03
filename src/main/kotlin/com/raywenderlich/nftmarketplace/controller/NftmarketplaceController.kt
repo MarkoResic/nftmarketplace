3456ac3c6857c32e899cdf538e28a3719fd1e048
Marko Rešić <mresic@racunarstvo.hr>
2023-10-03 10:00:21 +0200

    Move error handling code to NFTErrorHandler
    Methods for error handling have been removed from NftmarketplaceController and added to NftErrorHandler

--- src/main/kotlin/com/raywenderlich/nftmarketplace/controller/NftmarketplaceController.kt
+++ src/main/kotlin/com/raywenderlich/nftmarketplace/controller/NftmarketplaceController.kt
@@ -3,19 +3,10 @@ package com.raywenderlich.nftmarketplace.controller
 import com.raywenderlich.nftmarketplace.model.NFT
 import com.raywenderlich.nftmarketplace.service.NftService
 import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
 class NftmarketplaceController(private val service: NftService) {
-
-    @ExceptionHandler(NoSuchElementException::class)
-    fun handleNotFound(e: NoSuchElementException): ResponseEntity<String> =
-            ResponseEntity(e.message, HttpStatus.NOT_FOUND)
-
-    @ExceptionHandler(IllegalArgumentException::class)
-    fun handleBadRequest(e: IllegalArgumentException): ResponseEntity<String> =
-            ResponseEntity(e.message, HttpStatus.BAD_REQUEST)
 
     @GetMapping("/homepage")
     fun getHomePage() = "NFTs Marketplace"
---
+++ src/main/kotlin/com/raywenderlich/nftmarketplace/exception/NftErrorHandler.kt
@@ -0,0 +1,17 @@
+package com.raywenderlich.nftmarketplace.exception
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+
+@ControllerAdvice
+class NftErrorHandler {
+    @ExceptionHandler(NoSuchElementException::class)
+    fun handleNotFound(e: NoSuchElementException): ResponseEntity<String> =
+            ResponseEntity(e.message, HttpStatus.NOT_FOUND)
+
+    @ExceptionHandler(IllegalArgumentException::class)
+    fun handleBadRequest(e: IllegalArgumentException): ResponseEntity<String> =
+            ResponseEntity(e.message, HttpStatus.BAD_REQUEST)
+}
