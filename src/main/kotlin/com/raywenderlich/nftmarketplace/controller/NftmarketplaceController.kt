cde92e3f1c8f81b7216243a19a93b8c028253ca9
Marko Rešić <mresic@racunarstvo.hr>
2023-10-03 09:34:05 +0200

    Add exception handlers and REST methods to NftmarketplaceController
    - Exceptions now result in the application sending the appropriate HTTP response
    - User is now able to perform CRUD operations with NFTs using HTTP requests

--- src/main/kotlin/com/raywenderlich/nftmarketplace/controller/NftmarketplaceController.kt
+++ src/main/kotlin/com/raywenderlich/nftmarketplace/controller/NftmarketplaceController.kt
@@ -1,10 +1,39 @@
 package com.raywenderlich.nftmarketplace.controller
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
+import com.raywenderlich.nftmarketplace.model.NFT
+import com.raywenderlich.nftmarketplace.service.NftService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
 
 @RestController
-class NftmarketplaceController {
+class NftmarketplaceController(private val service: NftService) {
+
+    @ExceptionHandler(NoSuchElementException::class)
+    fun handleNotFound(e: NoSuchElementException): ResponseEntity<String> =
+            ResponseEntity(e.message, HttpStatus.NOT_FOUND)
+
+    @ExceptionHandler(IllegalArgumentException::class)
+    fun handleBadRequest(e: IllegalArgumentException): ResponseEntity<String> =
+            ResponseEntity(e.message, HttpStatus.BAD_REQUEST)
+
     @GetMapping("/homepage")
     fun getHomePage() = "NFTs Marketplace"
+
+    @GetMapping("/api")
+    fun getNFTs(): Collection<NFT> = service.getNFTs()
+
+    @GetMapping("/api/{id}")
+    fun getNFT(@PathVariable id: Int): NFT = service.getNFT(id)
+
+    @PostMapping("/api")
+    @ResponseStatus(HttpStatus.CREATED)
+    fun addNFT(@RequestBody nft: NFT): NFT = service.addNFT(nft)
+
+    @PatchMapping("/api")
+    fun updateNFT(@RequestBody nft: NFT): NFT = service.updateNFT(nft)
+
+    @DeleteMapping("/api/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun deleteNFT(@PathVariable id: Int) = service.deleteNFT(id)
 }
